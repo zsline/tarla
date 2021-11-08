764589ce8b29fa9e4e29e80fda30ea11771bd3da
zsline <sergei.dereka@gmal.com>
2021-11-08 19:05:05 +0200

    end

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,30 @@ $(function () {
 $('.home__slider').slick({
   fade: true,
 });
+  $('.tar__slider').slick({
+    centerMode: true,
+    centerPadding: '180px',
+    slidesToShow: 3,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '180px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '100px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 })
